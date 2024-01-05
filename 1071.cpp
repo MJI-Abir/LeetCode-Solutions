92e3c8fda3809bfe70c8c9ba3cb9bcfe6ebcfa2d
MJI-Abir <mjiabir12007@gmail.com>
2024-01-05 13:31:37 +0600

    solved 443 and 1071

--- 1071.cpp
+++ 1071.cpp
@@ -4,14 +4,20 @@ using namespace std;
 class Solution {
 public:
   string gcdOfStrings(string str1, string str2) {
-    
+    // Check if they have non-zero GCD string.
+    if(str1 + str2 != str2 + str1){
+      return "";
+    }
+    // Get the gcd of the two lengths
+    int gcdLength = gcd(str1.size(), str2.size());
+    return str1.substr(0, gcdLength);
   }
 };
 
 int main() {
-  string s;
-  cin >> s;
+  string s1, s2;
+  cin >> s1 >> s2;
   Solution solution;
-  cout << solution.reverseVowels(s) << endl;
+  cout << solution.gcdOfStrings(s1, s2) << endl;
   return 0;
 }
--- 443.cpp
+++ 443.cpp
@@ -4,25 +4,28 @@ using namespace std;
 class Solution {
 public:
   int compress(vector<char> &chars) {
-    int count = 1;
-    int j = 1;
-    for (int i = 1; i < chars.size(); i++) {
-      if (chars[i] == chars[i - 1]) {
-        count++;
-      } else {
-        chars[j] = (char)count;
-        j++;
-        chars[j] = chars[i];
-        count = 1;
+    int i = 0, compressedStringPointer = 0;
+    while (i < chars.size()) {
+      int groupLength = 1;
+
+      while (i + groupLength < chars.size() &&
+             chars[i + groupLength] == chars[i]) {
+        groupLength++;
       }
+      chars[compressedStringPointer++] = chars[i];
+      if (groupLength > 1) {
+        for (char character : toString(groupLength)) {
+          chars[compressedStringPointer++] = character;
+        }
+      }
+      i += groupLength;
     }
-    chars.erase(chars.end() - chars.size() + j + 1, chars.end());
-    return j + 1;
+    return compressedStringPointer;
   }
 };
 
 int main() {
-  vector<char> chars = {'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+  vector<char> chars = {'a'};
   Solution solution;
   cout << solution.compress(chars) << endl;
   for (char c : chars) {
